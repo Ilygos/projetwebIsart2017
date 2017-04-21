02a7bb2b09b6552ff10254754a1ac0e9b35cbaa5
DESKTOP-AHLI5F2\Maxence <maxence.beaumont6@gmail.com>
2017-04-21 11:43:06 +0200

    clear

--- scripts/Turn.js
+++ scripts/Turn.js
@@ -28,22 +28,6 @@
     }
 
 
-    function checkCards()
-    {
-        var p1CardType = Trun.that.cardInfo[0]["Type"];
-        var p2CardType = Trun.that.cardInfo[1]["Type"];
-        if ( TYPES.indexOf(p1CardType) == TYPES.indexOf(p1CardType))
-          meteor();
-        else if(TYPES.indexOf(p1CardType) == 0 && TYPES.indexOf(p2CardType) == TYPES.length-1)
-          playerHurt(1);
-        else if(TYPES.indexOf(p2CardType) == 0 && TYPES.indexOf(p1CardType) == TYPES.length-1)
-          playerHurt(2);
-        else if(TYPES.indexOf(p1CardType) > TYPES.indexOf(p2CardType))
-          playerHurt(1);
-        else if (TYPES.indexOf(p1CardType) < TYPES.indexOf(p2CardType))
-          playerHurt(2);
-    }
-
 
     function showResolve();
     {
