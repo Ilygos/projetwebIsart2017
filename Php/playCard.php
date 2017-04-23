5d71e672ff2d511a8414885c942220d485017866
DESKTOP-AHLI5F2\Maxence <maxence.beaumont6@gmail.com>
2017-04-23 23:31:31 +0200

    On progress

--- Php/playCard.php
+++ Php/playCard.php
@@ -7,10 +7,10 @@ $TYPES = ["High","Low","Mid"];
 
 function checkCards()
 {
-    var $p1CardType = $_SESSION["Turn"][0]["Type"];
-    var $p2CardType = $_SESSION["Turn"][1]["Type"];
+    var $p1CardType = $_SESSION["cardPlayed"][0]["Type"];
+    var $p2CardType = $_SESSION["cardPlayed"][1]["Type"];
     if ( $TYPES.indexOf($p1CardType) == $TYPES.indexOf($p1CardType))
-      meteor();
+      return "meteor";
     else if($TYPES.indexOf($p1CardType) == 0 && $TYPES.indexOf($p2CardType) == count($TYPES)-1)
       playerHurt(1);
     else if($TYPES.indexOf($p2CardType) == 0 && $TYPES.indexOf($p1CardType) == count($TYPES)-1)
@@ -19,7 +19,14 @@ function checkCards()
       playerHurt(1);
     else if ($TYPES.indexOf($p1CardType) < $TYPES.indexOf($p2CardType))
       playerHurt(2);
+    return "turnResolved";
 }
 
+
+function playerHurt($idPlayer)
+{
+  $newLife = $_SESSION["Player".$idPlayer]["Life"] - $_SESSION["Turn"][$idPlayer - 1]["Damage"];
+  $query = $bdd->prepare("UPDATE `Players` SET `Life`=".$newLife." WHERE `ID_Player`=".$idPlayer);
+}
 
 ?>
--- scripts/Turn.js
+++ scripts/Turn.js
@@ -1,4 +1,4 @@
-/*define(['jquery', 'Player'], function($, Player){
+define(['jquery', 'Player'], function($, Player){
     var urlPhpFile = "../Php/fetchTurn.php";
 
     function Turn()
@@ -13,54 +13,6 @@
         turn.that.infos = data;
     }
 
-
-    function failure(jqxhr, textStatus, error)
-		{
-  			console.log("doFailed :", jqxhr.status, textStatus, error);
-		}
-
-
-    function core()
-    {
-        if(turn.that.infos["HasResolved"])
-          fetchCardPlayed();
-        else
-    }
-
-
-
-    function showResolve();
-    {
-        checkCards();
-        if (!meteor) playTurn();
-        else playMeteor();
-    }
-
-
-    function fetchCardPlayed()
-    {
-        var cardsId = {player1Card:turn.that.infos["Player1CardID"], player2Card:turn.that.infos["Player2CardID"]};
-        $.ajax({
-          url : "../Php/fetchCardPlayed.php"
-          type : 'post',
-          data : cardsId,
-          dataType: 'json',
-          success : function(data)
-          {
-            turn.that.cardInfo = data;
-            showResolve();
-          },
-          error : failure
-        });
-    }
-
-
-    function playTurn()
-    {
-        alert("Your turn !");
-    }
-
-
     function init()
     {
         $.ajax({
@@ -74,4 +26,3 @@
 
 		return Turn;
 });
-*/
